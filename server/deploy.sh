fa65bcf486cb789118c79c444232cbff4e6e54dd
Willian Xavier <willian.rodrigues@intelitrader.com.br>
2023-01-15 23:26:44 -0300

    Add fetch from remote when deploying the server

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+BLUE_BOLD=$'\e[1;34m'
+RESET=$'\e[0m'
 my_dir=$(dirname $0)
 
 set -eo pipefail
 
+branch=$1
+if [ -z "$branch" ]; then
+        echo "Type the name of the branch you would like to deploy: "
+        read branch
+fi
+
+echo "Deploying branch ${BLUE}$branch${RESET}..."
+
 echo "Stoping service"
 systemctl stop http_server
+
+git fetch --all
+git reset --hard origin/$branch
+<<SCRIPT
+set -euo pipefail
+SCRIPT
 
 echo "Building binary"
 mkdir -p $my_dir/../bin
