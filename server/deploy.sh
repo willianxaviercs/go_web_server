b35c68155c54768461d7870c2f097b09d0d9d7a8
Willian Xavier <willian.rodrigues@intelitrader.com.br>
2023-01-16 01:32:46 -0300

    Increment deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-BLUE_BOLD=$'\e[1;34m'
-RESET=$'\e[0m'
-my_dir=$(dirname $0)
+BLUE_BOLD='\e[1;34m'
+RESET='\e[0m'
 
 set -eo pipefail
+
+do_as() {
+    sudo -u $1 --preserve-env=PATH bash -s
+}
 
 branch=$1
 if [ -z "$branch" ]; then
@@ -17,16 +20,22 @@ echo "Deploying branch ${BLUE}$branch${RESET}..."
 echo "Stoping service"
 systemctl stop http_server
 
-git fetch --all
-git reset --hard origin/$branch
-<<SCRIPT
-set -euo pipefail
+do_as devops <<'SCRIPT'
+    set -euo pipefail
+    cd /home/devops/http_server
+    git fetch --all
+    git reset --hard origin/$branch
 SCRIPT
 
 echo "Building binary"
-mkdir -p $my_dir/../bin
-go build -o $my_dir/../bin/http_server $my_dir/../src/main.go
 
+do_as devops <<'SCRIPT'
+    set -euo pipefail
+    cd /home/devops/http_server/
+    mkdir -p bin
+    go build -o ./bin/http_server ./src/main.go
+SCRIPT
 
 echo "Starting service"
+systemctl daemon-reload
 systemctl start http_server
